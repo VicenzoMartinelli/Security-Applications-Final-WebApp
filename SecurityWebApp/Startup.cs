a9f65a1d423f641c5220b983d0b4459279c49933
VicenzoMartinelli <vicenzomartinelli@hotmail.com>
2019-05-22 19:34:17 -0300

    Add project files.

---
+++ SecurityWebApp/Startup.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecurityWebApp.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+
+namespace SecurityWebApp
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.Configure<CookiePolicyOptions>(options =>
+      {
+        options.CheckConsentNeeded = context => true;
+        options.MinimumSameSitePolicy = SameSiteMode.None;
+      });
+
+      services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DbSqlServer")));
+
+      services.AddDefaultIdentity<IdentityUser>()
+          .AddDefaultUI(UIFramework.Bootstrap4)
+          .AddDefaultTokenProviders()
+          .AddEntityFrameworkStores<ApplicationDbContext>();
+
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+          .AddRazorPagesOptions(options =>
+          {
+            options.AllowAreas = true;
+            options.Conventions.ConfigureFilter(new IgnoreAntiforgeryTokenAttribute());
+            options.Conventions.AuthorizeAreaFolder("Identity", "/Account/Manage");
+            options.Conventions.AuthorizeAreaPage("Identity", "/Account/Logout");
+          })
+          .AddMvcOptions((x) => {
+            x.CacheProfiles.Add(new string("lel"), new CacheProfile()
+            {
+              Duration = 5000
+            });
+          });
+
+      services.ConfigureApplicationCookie(options =>
+      {
+        options.LoginPath = $"/Identity/Account/Login";
+        options.LogoutPath = $"/Identity/Account/Logout";
+        options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+      });
+    }
+
+    
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      var cultureInfo = new CultureInfo("pt-BR");
+
+      CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+      CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
+      var supportedCultures = new[] { cultureInfo };
+      app.UseRequestLocalization(new RequestLocalizationOptions
+      {
+        DefaultRequestCulture = new RequestCulture(culture: "pt-BR", uiCulture: "pt-BR"),
+        SupportedCultures     = supportedCultures,
+        SupportedUICultures   = supportedCultures
+      });
+
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseDatabaseErrorPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+      }
+
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+      app.UseCookiePolicy();
+
+      app.UseAuthentication();
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+                  name: "default",
+                  template: "{controller=Home}/{action=Index}/{id?}");
+      });
+    }
+  }
+}
